75255d73a74d3b858ed06156feacd92d59ac26c9
Sebastian <sebastian.ruzanowski@gmail.com>
2019-08-22 01:21:19 +0200

    added manufacturer commands

---
+++ Services/Product/U.ProductService.Application/Manufacturers/Commands/Create/CreateManufacturerCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MediatR;
+using Newtonsoft.Json;
+
+namespace U.ProductService.Application.Manufacturers.Commands.Create
+{
+    public class CreateManufacturerCommand : IRequest<Guid>
+    {
+        public string Name { get;  set; }
+        public string Description { get;  set; }
+
+        [JsonConstructor]
+        public CreateManufacturerCommand()
+        {
+                
+        }
+
+        public CreateManufacturerCommand(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
---
+++ Services/Product/U.ProductService.Application/Manufacturers/Commands/Create/CreateManufacturerCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using U.ProductService.Domain;
+
+namespace U.ProductService.Application.Manufacturers.Commands.Create
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public class CreateManufacturerCommandHandler : IRequestHandler<CreateManufacturerCommand, Guid>
+    {
+        private readonly IManufacturerRepository _manufacturerRepository;
+
+        public CreateManufacturerCommandHandler(IManufacturerRepository manufacturerRepository)
+        {
+            _manufacturerRepository = manufacturerRepository ?? throw new ArgumentNullException(nameof(manufacturerRepository));
+        }
+
+        public async Task<Guid> Handle(CreateManufacturerCommand command, CancellationToken cancellationToken)
+        {
+            var product = GetProduct(command);
+
+            await _manufacturerRepository.AddAsync(product);
+            await _manufacturerRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
+
+            return product.Id;
+        }
+
+        private Manufacturer GetProduct(CreateManufacturerCommand command)
+        {
+            return new Manufacturer(Guid.NewGuid(),
+                command.Name,
+                command.Description);
+        }
+    }
+}
