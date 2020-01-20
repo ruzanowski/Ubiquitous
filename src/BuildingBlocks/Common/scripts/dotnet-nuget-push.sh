5d8dd6fef474de2e287642f90c880c52fb3151d9
Sebastian <sebastian.ruzanowski@gmail.com>
2020-01-20 22:41:08 +0100

    attempt to fix nuget push

--- src/BuildingBlocks/Common/scripts/dotnet-nuget-push.sh
+++ src/BuildingBlocks/Common/scripts/dotnet-nuget-push.sh
@@ -17,6 +17,6 @@ case "$CI_COMMIT_REF_NAME" in
     dotnet nuget push *.nupkg -k ${MYGET_API_KEY} -source "https://www.myget.org/F/ubiquitous/api/v2/package"
     ;;
   * | "develop")
-    dotnet nuget push *.nupkg -s ${MYGET_URL_DEVELOP} -k ${MYGET_API_KEY}
+    dotnet nuget push --force-english-output -s "https://www.myget.org/F/ubiquitous-develop/api/v2/package" -k "${MYGET_API_KEY}" ./*.nupkg
     ;;
 esac
