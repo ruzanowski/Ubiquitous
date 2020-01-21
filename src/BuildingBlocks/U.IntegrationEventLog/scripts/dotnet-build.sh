6e2f36a486054cbfa725dcb24e238c6f2d641922
Sebastian <sebastian.ruzanowski@gmail.com>
2020-01-21 02:31:27 +0100

    fix with merging

--- src/BuildingBlocks/U.IntegrationEventLog/scripts/dotnet-build.sh
+++ src/BuildingBlocks/U.IntegrationEventLog/scripts/dotnet-build.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+case "$CI_COMMIT_REF_NAME" in
+  "master")
+  dotnet restore -s https://www.myget.org/F/ubiquitous/api/v2/package
+    ;;
+  * | "develop")
+  dotnet restore -s https://www.myget.org/F/ubiquitous-develop/api/v2/package
+    ;;
+esac
+
 dotnet build -c Release --no-cache
