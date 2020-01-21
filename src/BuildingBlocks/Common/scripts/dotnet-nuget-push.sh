56d39e468dde9bb4a1035e73fa457d0b5d36b2f4
Sebastian <sebastian.ruzanowski@gmail.com>
2020-01-21 01:54:26 +0100

    attempt to fix nuget push

--- src/BuildingBlocks/Common/scripts/dotnet-nuget-push.sh
+++ src/BuildingBlocks/Common/scripts/dotnet-nuget-push.sh
@@ -1,20 +1,12 @@
-#!/bin/sh
-echo Currently in
-pwd
-ls
-
+#!/bin/bash
 dotnet pack /p:PackageVersion=${UBIQUITOUS_VERSION}.${CI_JOB_ID} --no-restore -o .
-
 echo Uploading package to MyGet using branch $CI_COMMIT_REF_NAME
-echo Currently in
-pwd
-ls
 
 case "$CI_COMMIT_REF_NAME" in
   "master")
-    dotnet nuget push *.nupkg -k ${MYGET_API_KEY} -source "https://www.myget.org/F/ubiquitous/api/v2/package"
+  dotnet nuget push *.nupkg -k $MYGET_API_KEY -s https://www.myget.org/F/ubiquitous/api/v2/package
     ;;
   * | "develop")
-    dotnet nuget push *.nupkg -s https://www.myget.org/F/ubiquitous-develop/api/v2/package -k ${MYGET_API_KEY}
+  dotnet nuget push *.nupkg -k $MYGET_API_KEY -s https://www.myget.org/F/ubiquitous-develop/api/v2/package
     ;;
 esac
