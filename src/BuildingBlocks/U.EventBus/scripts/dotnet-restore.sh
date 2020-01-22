7e803054d63fa21e5d9bc35ba67bfdc4bf367b6a
Sebastian <sebastian.ruzanowski@gmail.com>
2020-01-22 23:18:42 +0100

    property configuration release

--- src/BuildingBlocks/U.EventBus/scripts/dotnet-restore.sh
+++ src/BuildingBlocks/U.EventBus/scripts/dotnet-restore.sh
@@ -4,6 +4,6 @@ case "$CI_COMMIT_REF_NAME" in
   dotnet restore /property:Configuration=Release -s https://www.myget.org/F/ubiquitous/api/v3/index.json -s https://api.nuget.org/v3/index.json
     ;;
   * | "develop")
-  dotnet restore /property:Configuration=Release -s https://www.myget.org/F/ubiquitous-develop/api/v3/index.json -s https://api.nuget.org/v3/index.json
+  dotnet restore /property:Configuration=Release -s "https://www.myget.org/F/ubiquitous-develop/api/v3/index.json" -s "https://api.nuget.org/v3/index.json"
     ;;
 esac
