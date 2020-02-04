320b29d301e433f71e9660f6f540fe351044cb91
Sebastian <sebastian.ruzanowski@gmail.com>
2020-02-05 00:23:21 +0100

    adapters, services final attempt

--- src/Adapters/U.SmartStoreAdapter/scripts/docker-publish.sh
+++ src/Adapters/U.SmartStoreAdapter/scripts/docker-publish.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 DOCKER_TAG=''
 NUGET_CONFIG_FILE=''
-PORT=''
-PROJECT_NAME=''
 
 case "$CI_COMMIT_REF_NAME" in
   "master")
@@ -21,5 +19,5 @@ apk upgrade
 apk add python python-dev py-pip build-base libffi-dev openssl-dev
 pip install docker-compose
 docker login -u $CI_REGISTRY_USER -p "$CI_REGISTRY_PASSWORD" $CI_REGISTRY
-docker build --build-arg PROJECT_NAME=${PROJECT_NAME} --build-arg PORT=${PORT} --build-arg NUGET_CONFIG_FILE=${NUGET_CONFIG_FILE} -t registry.gitlab.com/ruzanowski/ubiquitous/smartstoreadapter:$DOCKER_TAG .
+docker build --build-arg NUGET_CONFIG_FILE=${NUGET_CONFIG_FILE} -t registry.gitlab.com/ruzanowski/ubiquitous/smartstoreadapter:$DOCKER_TAG .
 docker push registry.gitlab.com/ruzanowski/ubiquitous/smartstoreadapter:$DOCKER_TAG
