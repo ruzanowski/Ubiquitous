524b5bd511bf93a54fb5d21ed7da66adacb0451d
Sebastian <sebastian.ruzanowski@gmail.com>
2020-05-06 00:37:06 +0200

    Environment variables missing for production

--- src/U.WebApp.Angular/src/environments/environment.prod.ts
+++ src/U.WebApp.Angular/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  hostIP: '77.55.216.216',
+  apiGatewayPort: '4500',
+  productServicePort: '5000',
+  smartStoreAdapterPort: '5100',
+  fetchServicePort: '5200',
+  subscriptionServicePort: '5300',
+  identityServicePort: '5400',
+  notificationServicePort: '5500',
+  signalrPort: '5500',
+  signalrEndpoint: 'signalr'
 };
