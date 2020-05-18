8f2f4406537e6b702473c5bc5690983599908228
Sebastian <sebastian.ruzanowski@gmail.com>
2020-05-18 22:21:38 +0200

    nuget to the same folder as scripts, amendments to gitlab.yml

--- src/BuildingBlocks/U.IntegrationEventLog/scripts/dotnet-restore.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-case "$CI_COMMIT_REF_NAME" in
-  "master")
-  dotnet restore /property:Configuration=Release --configfile ../nuget.config --no-cache
-    ;;
-  * | "develop")
-  dotnet restore /property:Configuration=Release --configfile ../nuget.develop.config --no-cache
-    ;;
-esac
