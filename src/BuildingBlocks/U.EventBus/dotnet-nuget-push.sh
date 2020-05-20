ebef1e5b0e09f46b64cc707dcda891b0334d6605
Sebastian <sebastian.ruzanowski@gmail.com>
2020-05-20 18:44:17 +0200

    no-build, no-restore during packing a nuget

--- src/BuildingBlocks/U.EventBus/dotnet-nuget-push.sh
+++ src/BuildingBlocks/U.EventBus/dotnet-nuget-push.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-dotnet pack -c Release /p:Version=${UBIQUITOUS_VERSION}.${CI_PIPELINE_IID} -o .
+dotnet pack --no-build --no-restore -c Release /p:Version=${UBIQUITOUS_VERSION}.${CI_PIPELINE_IID} -o .
 
 echo Uploading package to MyGet using branch $CI_COMMIT_REF_NAME
 
 case "$CI_COMMIT_REF_NAME" in
   "master")
-    dotnet nuget push *.nupkg -k $MYGET_API_KEY -s https://www.myget.org/F/ubiquitous/api/v2/package --no-restore
+    dotnet nuget push *.nupkg -k $MYGET_API_KEY -s https://www.myget.org/F/ubiquitous/api/v2/package
     ;;
   * | "develop")
-    dotnet nuget push *.nupkg -k $MYGET_API_KEY -s https://www.myget.org/F/ubiquitous-develop/api/v2/package --no-restore
+    dotnet nuget push *.nupkg -k $MYGET_API_KEY -s https://www.myget.org/F/ubiquitous-develop/api/v2/package
     ;;
 esac
